68e5e47b2fc1f239003c35b7d9701f0653cd895f
OryzaAmaliatuzZahra <127116057+OryzaAmaliatuzZahra@users.noreply.github.com>
2023-03-17 14:47:23 +0700

    daily-task-3 commit

---
+++ daily-task-3.js
@@ -0,0 +1,343 @@
+let person = [
+    {
+        "_id": "64105621928231abf68e3ca7",
+        "age": 24,
+        "eyeColor": "blue",
+        "name": "Odom Joyce",
+        "address": "Bali",
+        "registered": "2020-04-11T07:13:05 -07:00",
+        "friends": [
+            {
+                "id": 0,
+                "name": "Theresia",
+                "isActive": false,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 2,
+                        "hobby": "basketball"
+                    },
+                    {
+                        "id": 1,
+                        "hobby": "basketball"
+                    }
+                ]
+            },
+            {
+                "id": 1,
+                "name": "Theresia",
+                "isActive": true,
+                "favoriteFruit": "banana",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 2,
+                        "hobby": "basketball"
+                    },
+                    {
+                        "id": 2,
+                        "hobby": "basketball"
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Theresia",
+                "isActive": false,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 1,
+                        "hobby": "baseball"
+                    },
+                    {
+                        "id": 2,
+                        "hobby": "basketball"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "_id": "64105621a679ef2adb1d0142",
+        "age": 30,
+        "eyeColor": "green",
+        "name": "Mcmillan Floyd",
+        "address": "Bandung",
+        "registered": "2022-10-05T12:29:05 -07:00",
+        "friends": [
+            {
+                "id": 0,
+                "name": "Shifly",
+                "isActive": true,
+                "favoriteFruit": "apple",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 1,
+                        "hobby": "basketball"
+                    },
+                    {
+                        "id": 1,
+                        "hobby": "baseball"
+                    }
+                ]
+            },
+            {
+                "id": 1,
+                "name": "Shifly",
+                "isActive": true,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 2,
+                        "hobby": "football"
+                    },
+                    {
+                        "id": 2,
+                        "hobby": "baseball"
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Farid",
+                "isActive": true,
+                "favoriteFruit": "banana",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 2,
+                        "hobby": "football"
+                    },
+                    {
+                        "id": 2,
+                        "hobby": "baseball"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "_id": "64105621ba7ecb16e663f3a5",
+        "age": 30,
+        "eyeColor": "blue",
+        "name": "Meyers Mckinney",
+        "address": "Bali",
+        "registered": "2021-04-25T03:20:24 -07:00",
+        "friends": [
+            {
+                "id": 0,
+                "name": "Theresia",
+                "isActive": true,
+                "favoriteFruit": "apple",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 2,
+                        "hobby": "football"
+                    },
+                    {
+                        "id": 1,
+                        "hobby": "basketball"
+                    }
+                ]
+            },
+            {
+                "id": 1,
+                "name": "Shifly",
+                "isActive": false,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 1,
+                        "hobby": "basketball"
+                    },
+                    {
+                        "id": 1,
+                        "hobby": "baseball"
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Theresia",
+                "isActive": true,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 1,
+                        "hobby": "baseball"
+                    },
+                    {
+                        "id": 2,
+                        "hobby": "football"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "_id": "64105621b5bec66f58441e7d",
+        "age": 26,
+        "eyeColor": "green",
+        "name": "Sexton Kinney",
+        "address": "Bali",
+        "registered": "2020-05-06T11:06:18 -07:00",
+        "friends": [
+            {
+                "id": 0,
+                "name": "Shifly",
+                "isActive": true,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 1,
+                        "hobby": "baseball"
+                    },
+                    {
+                        "id": 2,
+                        "hobby": "football"
+                    }
+                ]
+            },
+            {
+                "id": 1,
+                "name": "Farid",
+                "isActive": true,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 1,
+                        "hobby": "basketball"
+                    },
+                    {
+                        "id": 2,
+                        "hobby": "baseball"
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Theresia",
+                "isActive": false,
+                "favoriteFruit": "banana",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 1,
+                        "hobby": "baseball"
+                    },
+                    {
+                        "id": 1,
+                        "hobby": "basketball"
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "_id": "6410562170b4c30f22bad69b",
+        "age": 29,
+        "eyeColor": "green",
+        "name": "Simpson Golden",
+        "address": "Jakarta",
+        "registered": "2020-03-12T10:20:02 -07:00",
+        "friends": [
+            {
+                "id": 0,
+                "name": "Farid",
+                "isActive": true,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 2,
+                        "hobby": "basketball"
+                    },
+                    {
+                        "id": 1,
+                        "hobby": "basketball"
+                    }
+                ]
+            },
+            {
+                "id": 1,
+                "name": "Farid",
+                "isActive": true,
+                "favoriteFruit": "banana",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 2,
+                        "hobby": "baseball"
+                    },
+                    {
+                        "id": 1,
+                        "hobby": "baseball"
+                    }
+                ]
+            },
+            {
+                "id": 2,
+                "name": "Shifly",
+                "isActive": true,
+                "favoriteFruit": "strawberry",
+                "gender": "male",
+                "hobby": [
+                    {
+                        "id": 2,
+                        "hobby": "football"
+                    },
+                    {
+                        "id": 1,
+                        "hobby": "football"
+                    }
+                ]
+            }
+        ]
+    }
+]
+
+// 1) display/print person yang registered dibawah tahun 2022
+console.log("======================= No 1 ==========================")
+function checkRegis(registered) {
+    return registered.substring(0, 4) < 2022
+}
+
+function loopingRegis (regis) {
+    let arrPerson = []
+    for (let i = 0; i < regis.length; i++) {
+        if(checkRegis(regis[i].registered)) {
+            arrPerson.push (regis[i])
+        }
+    }
+    return arrPerson
+}
+console.log(loopingRegis(person))
+
+// 2) display person yang address nya Bali 
+console.log("")
+console.log("")
+console.log("========================= No 2 =========================")
+function checkAddress(address) {
+    return address === "Bali"
+}
+
+function loopingPerson(persons) {
+    let arrPerson = []
+    for (let i = 0; i < persons.length; i++) {
+        if (checkAddress(persons[i].address)) {
+            arrPerson.push(persons[i])
+        }
+    }
+    return arrPerson
+}
+console.log(loopingPerson(person))
